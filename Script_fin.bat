bba9103940542a91682a812a68b6f6ddc668577c
Anriker <85021991+Anriker@users.noreply.github.com>
2022-05-26 15:54:59 +0300

    Финальный бат файл

---
+++ Script_fin.bat
@@ -0,0 +1,20 @@
+echo off
+md Filippov
+cd Filippov
+md Aleksandr
+md Alekseyvich
+pause
+echo > 05062001.txt
+cd Alekseyvich
+echo > computer_011.txt
+pause
+cd ..
+cd ..
+del Filippov /S /Q /F
+pause
+cd Filippov
+rd Alekseyvich
+rd Aleksandr
+cd ..
+rd Filippov
+pause
